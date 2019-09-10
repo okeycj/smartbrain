d5bca45dbcc5a4659709266f5a0e0412c7fc3eed
Azubuike Chijindu Okechukwu <30808024+okeycj@users.noreply.github.com>
2019-09-10 22:17:25 +0100

    Updates

---
+++ precache-manifest.6d3518d949c4c283b699f819109f3e11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032cc875f7961963654dcb511a655dd3",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "fb9cdc314c8645ed032d",
+    "url": "/smartbrain/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "f10b7b0b89d2dee0118a",
+    "url": "/smartbrain/static/css/main.5ab20212.chunk.css"
+  },
+  {
+    "revision": "fb9cdc314c8645ed032d",
+    "url": "/smartbrain/static/js/2.08c38882.chunk.js"
+  },
+  {
+    "revision": "f10b7b0b89d2dee0118a",
+    "url": "/smartbrain/static/js/main.c645d732.chunk.js"
+  },
+  {
+    "revision": "9ac85057d7b96f435469",
+    "url": "/smartbrain/static/js/runtime~main.e0aa727b.js"
+  }
+]);
